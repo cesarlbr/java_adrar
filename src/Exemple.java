18069af544a780ce845aee9cebca9b9999460431
cesarlbr <cesar.labrunie@gmail.com>
2024-02-12 11:03:20 +0100

    first commit

---
+++ src/Exemple.java
@@ -0,0 +1,12 @@
+public class Exemple {
+    public String nom;
+    public String prenom;
+    public Exemple(){};
+    public Exemple(String lastname,String firstname){
+        this.nom = lastname;
+        this.prenom = firstname;
+    }
+    public String toString(){
+      return this.nom + " " + this.prenom;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Exemple test = new Exemple();
+        Exemple test2 = new Exemple("labrunie", "cesar");
+        System.out.println(test2.nom + " " + test2.prenom);
+
+
+    }
+}
